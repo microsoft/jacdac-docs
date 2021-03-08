f24e81e46b0dd60743f54d27f7125cf7b6303cec
pelikhan <jhalleux@microsoft.com>
2021-03-08 22:48:56 +0100

    removed mdx renderer

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -9,8 +9,6 @@ import { DRAWER_WIDTH, MOBILE_BREAKPOINT, TOC_DRAWER_WIDTH } from "./layout";
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import AppContext, { DrawerType } from "./AppContext";
-import { useStaticQuery, graphql } from "gatsby";
-import Mdx from "./ui/Mdx";
 import PacketRecorder from "./PacketRecorder";
 import DrawerSearchInput from "./DrawerSearchInput";
 import DrawerSearchResults from "./DrawerSearchResults";
@@ -64,18 +62,6 @@ export default function AppDrawer(props: {
     const { drawerType, setDrawerType, searchQuery } = useContext(AppContext)
     const open = drawerType !== DrawerType.None
     const showSearchResults = !!searchQuery;
-    const query = useStaticQuery(graphql`
-        {
-          allFile(filter: {name: {eq: "service-specification"}}) {
-            nodes {
-              childMdx {
-                body
-              }
-            }
-          }
-        }
-      `)
-    const specMarkdown = query.allFile.nodes[0].childMdx.body
 
     const handleDrawerClose = () => {
         setDrawerType(DrawerType.None)
@@ -85,7 +71,6 @@ export default function AppDrawer(props: {
         return <></>
 
     const toc = drawerType === DrawerType.Toc;
-    const spec = drawerType === DrawerType.ServiceSpecification;
     return <Drawer
         className={toc ? classes.tocDrawer : classes.drawer}
         variant="persistent"
@@ -97,11 +82,10 @@ export default function AppDrawer(props: {
     >
         <div className={classes.drawerHeader}>
             {toc && <div className={classes.fluid}><DrawerSearchInput /></div>}
-            {!toc && !spec && <><PacketRecorder />
+            {!toc && <><PacketRecorder />
                 <span className={classes.fluid} />
                 <DrawerToolsButtonGroup showConnect={true} />
             </>}
-            {spec && <span className={classes.fluid} />}
             <IconButton aria-label="Collapse" onClick={handleDrawerClose}>
                 <ChevronLeftIcon />
             </IconButton>
@@ -109,7 +93,6 @@ export default function AppDrawer(props: {
         <Divider />
         {showSearchResults && <DrawerSearchResults />}
         {!showSearchResults && drawerType === DrawerType.Toc && <Toc pagePath={pagePath} />}
-        {!showSearchResults && drawerType == DrawerType.ServiceSpecification && <div className={classes.mdx}><Mdx mdx={specMarkdown} /></div>}
         {!showSearchResults && drawerType === DrawerType.Packets
             ? <PacketView showTime={true} />
             : drawerType === DrawerType.Dom ? <JDomTreeView /> : undefined}
