bbc0f1820cfbecc2019a5f428cd05e04a8867326
pelikhan <jhalleux@microsoft.com>
2021-05-20 06:59:08 -0700

    playing with blocks

--- src/pages/tools/vm-editor.tsx
+++ src/pages/tools/vm-editor.tsx
@@ -12,21 +12,185 @@ const useStyles = makeStyles(() =>
     })
 )
 
-const initialXml =
-    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>'
-const toolboxCategories = [
+const initialXml = '<xml xmlns="http://www.w3.org/1999/xhtml"></xml>'
+const blocks = [
     {
-        type: "controls_if",
+        type: "jacdac_declare_service",
+        message0: "service %1",
+        args0: [
+            {
+                type: "field_dropdown",
+                name: "TYPE",
+                options: [
+                    ["button", "button"],
+                    ["servo", "servo"],
+                    ["settings", "settings"],
+                ],
+            },
+        ],
+        inputsInline: true,
+        style: "variable_blocks",
+        tooltip: "declares a new service",
+        output: "button",
+        helpUrl: "",
     },
     {
-        type: "logic_compare",
+        type: "jacdac_button_event",
+        message0: "when %1 %2 %3",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "button",
+            },
+            {
+                type: "input_dummy",
+            },
+            {
+                type: "field_dropdown",
+                name: "EVENT",
+                options: [
+                    ["down", "down"],
+                    ["up", "up"],
+                    ["press", "press"],
+                ],
+            },
+        ],
+        style: "logic_blocks",
+        inputsInline: true,
+        nextStatement: null,
+        tooltip: "",
+        helpUrl: "",
     },
     {
-        type: "math_round",
+        type: "jacdac_reading_change",
+        message0: "when %1 change",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "humidity",
+            },
+        ],
+        inputsInline: true,
+        nextStatement: null,
+        style: "logic_blocks",
+        tooltip: "",
+        helpUrl: "",
     },
     {
-        type: "math_number",
+        type: "jacdac_await_condition",
+        message0: "when %1",
+        args0: [
+            {
+                type: "input_value",
+                name: "CONDITION",
+                check: "Boolean",
+            },
+        ],
+        style: "logic_blocks",
+        inputsInline: true,
+        nextStatement: null,
+        tooltip: "",
+        helpUrl: "",
     },
+    {
+        type: "jacdac_button_reading",
+        message0: "%1 pressure",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "button",
+            },
+        ],
+        inputsInline: true,
+        output: "Number",
+        colour: 230,
+        tooltip: "",
+        helpUrl: "",
+    },
+    {
+        type: "jacdac_humidity_reading",
+        message0: "%1 humidity",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "humidity",
+            },
+        ],
+        inputsInline: true,
+        output: "Number",
+        colour: 230,
+        tooltip: "",
+        helpUrl: "",
+    },
+    {
+        type: "jacdac_servo_write_angle",
+        message0: "set %1 angle to %2 %3",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "servo",
+            },
+            {
+                type: "input_dummy",
+            },
+            {
+                type: "input_value",
+                name: "VALUE",
+                check: "Number",
+            },
+        ],
+        inputsInline: true,
+        previousStatement: null,
+        nextStatement: null,
+        colour: 230,
+        tooltip: "",
+        helpUrl: "",
+    },
+    {
+        type: "jacdac_servo_enable",
+        message0: "%1 %2",
+        args0: [
+            {
+                type: "field_variable",
+                name: "ROLE",
+                variable: "servo",
+            },
+            {
+                type: "field_dropdown",
+                name: "ENABLED",
+                options: [
+                    ["on", "ON"],
+                    ["off", "OFF"],
+                ],
+            },
+        ],
+        colour: 230,
+        tooltip: "",
+        helpUrl: "",
+    },
+]
+blocks.map(
+    block =>
+        (Blockly.Blocks[block.type] = {
+            init: function () {
+                this.jsonInit(block)
+            },
+        })
+)
+const toolboxBlocks = [
+    { type: "variables_set" },
+    ...blocks.map(block => ({ type: block.type })),
+    { type: "text" },
+    { type: "math_number" },
+    { type: "math_arithmetic" },
+    { type: "math_single" },
+    { type: "logic_boolean" },
+    { type: "logic_compare" },
 ]
 
 export default function VMEditor() {
@@ -44,8 +208,10 @@ export default function VMEditor() {
     return (
         <>
             <ReactBlockly
-                toolboxBlocks={toolboxCategories}
+                toolboxBlocks={toolboxBlocks}
                 workspaceConfiguration={{
+                    comments: false,
+                    css: true,
                     grid: {
                         spacing: 25,
                         length: 1,
@@ -54,6 +220,7 @@ export default function VMEditor() {
                     },
                     renderer: "zelos",
                     theme: Theme,
+                    oneBasedIndex: false,
                 }}
                 initialXml={initialXml}
                 wrapperDivClassName={classes.editor}
