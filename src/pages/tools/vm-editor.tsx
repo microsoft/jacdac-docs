90e353a74c236ad48722dc09498bdf09d8d8bae2
pelikhan <jhalleux@microsoft.com>
2021-05-20 23:10:47 -0700

    add comment

--- src/pages/tools/vm-editor.tsx
+++ src/pages/tools/vm-editor.tsx
@@ -2,6 +2,7 @@ import { createStyles, Grid, makeStyles, NoSsr } from "@material-ui/core"
 import React from "react"
 import VmEditor from "../../components/blockly/VmEditor"
 import Dashboard from "../../components/dashboard/Dashboard"
+import Alert from "../../components/ui/Alert"
 import useLocalStorage from "../../components/useLocalStorage"
 
 const useStyles = makeStyles(() =>
@@ -24,8 +25,18 @@ export default function Page() {
     return (
         <Grid container direction="column" spacing={1}>
             <Grid item xs={12}>
+                <Alert severity="info">
+                    Start a simulator or connect a device to load the blocks
+                    automatically.
+                </Alert>
+            </Grid>
+            <Grid item xs={12}>
                 <NoSsr>
-                    <VmEditor className={classes.editor} initialXml={xml} onXmlChange={handleXml} />
+                    <VmEditor
+                        className={classes.editor}
+                        initialXml={xml}
+                        onXmlChange={handleXml}
+                    />
                 </NoSsr>
             </Grid>
             <Grid item xs={12}>
