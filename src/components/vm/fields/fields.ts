ec8c325180bd589d70b6de6451e36f281cc68679
pelikhan <jhalleux@microsoft.com>
2021-05-29 11:27:28 -0700

    mroe renaming

--- src/components/vm/fields/fields.ts
+++ src/components/vm/fields/fields.ts
@@ -6,7 +6,7 @@ import ServoAngleField from "./ServoAngleField"
 import { BlockDefinition } from "../toolbox"
 import { assert } from "../../../../jacdac-ts/src/jdom/utils"
 import LEDColorField from "./LEDColorField"
-import DashboardServiceField from "./DashboardServiceField"
+import TwinField from "./TwinField"
 
 let reactFieldShadows: BlockDefinition[]
 export function registerFields() {
@@ -27,7 +27,7 @@ export function registerFields() {
         LEDMatrixField,
         ServoAngleField,
         LEDColorField,
-        DashboardServiceField,
+        TwinField,
     ]
     fieldTypes.forEach(registerType)
 }
