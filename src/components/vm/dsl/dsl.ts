86686ce9342cfb82925d7a1fa7396012f8ad4574
pelikhan <jhalleux@microsoft.com>
2021-06-08 08:44:35 -0700

    refactoring jacdac services into dsl

--- src/components/vm/dsl/dsl.ts
+++ src/components/vm/dsl/dsl.ts
@@ -1,26 +1,26 @@
 import { Theme } from "@material-ui/core"
 import { Block, Workspace } from "blockly"
+import { JDService } from "../../../../jacdac-ts/src/jdom/service"
 import { RoleEvent } from "../../../../jacdac-ts/src/vm/compile"
-import { VMError } from "../../../../jacdac-ts/src/vm/ir"
+import { VMError, VMProgram } from "../../../../jacdac-ts/src/vm/ir"
 import { BlockJSON, WorkspaceJSON } from "../jsongenerator"
 import {
     BlockDefinition,
-    CategoryDefinition,
+    ContentDefinition,
     ServiceBlockDefinition,
 } from "../toolbox"
 import { ExpressionWithErrors } from "../VMgenerator"
 
 export default interface BlockDomainSpecificLanguage {
     id: string
-    createBlocks?: (options: {
-        theme: Theme
-        supportedServices: jdspec.ServiceSpec[]
-    }) => BlockDefinition[]
+    createBlocks?: (options: { theme: Theme, supportedServices }) => BlockDefinition[]
 
     createCategory?: (options: {
         theme: Theme
         source: WorkspaceJSON
-    }) => CategoryDefinition[]
+        program: VMProgram
+        liveServices: JDService[]
+    }) => ContentDefinition[]
 
     convertToJSON?: (options: {
         workspace: Workspace
