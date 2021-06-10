bb0d203b873d96764998fbc038b2f5d96bab4048
pelikhan <jhalleux@microsoft.com>
2021-06-09 18:49:07 -0700

    more cleanup

--- src/components/vm/VMEditor.tsx
+++ src/components/vm/VMEditor.tsx
@@ -1,14 +1,9 @@
 import { Grid, NoSsr } from "@material-ui/core"
-import React, { useContext, useEffect, useMemo, useRef, useState } from "react"
+import React, { useContext, useEffect, useMemo, useState } from "react"
 import Flags from "../../../jacdac-ts/src/jdom/flags"
-import { WorkspaceJSON } from "../blockly/jsongenerator"
-import VMBlockEditor from "./VMBlockEditor"
-import useLocalStorage from "../useLocalStorage"
 import useVMRunner from "./useVMRunner"
-import useRoleManager from "../hooks/useRoleManager"
 import VMDiagnostics from "./VMDiagnostics"
 import VMToolbar from "./VMToolbar"
-import { WorkspaceSvg } from "blockly"
 import { VMProgram } from "../../../jacdac-ts/src/vm/ir"
 import BlockContext, { BlockProvider } from "../blockly/BlockContext"
 import BlockDiagnostics from "../blockly/BlockDiagnostics"
@@ -27,13 +22,12 @@ import BlockEditor from "../blockly/BlockEditor"
 
 const VM_SOURCE_STORAGE_KEY = "tools:vmeditor"
 function VMEditorWithContext() {
-    const { dsls, workspace, workspaceXml, workspaceJSON, roleManager } =
+    const { dsls, workspace, workspaceJSON, roleManager } =
         useContext(BlockContext)
     const [program, setProgram] = useState<VMProgram>()
     const autoStart = true
     const { runner, run, cancel } = useVMRunner(roleManager, program, autoStart)
 
-    useEffect(() => setXml(workspaceXml), [workspaceXml])
     useEffect(() => {
         try {
             const newProgram = workspaceJSONToVMProgram(workspaceJSON, dsls)
@@ -61,13 +55,7 @@ function VMEditorWithContext() {
     return (
         <Grid container direction="column" spacing={1}>
             <Grid item xs={12}>
-                <VMToolbar
-                    roleManager={roleManager}
-                    runner={runner}
-                    run={run}
-                    cancel={cancel}
-                    program={program}
-                />
+                <VMToolbar runner={runner} run={run} cancel={cancel} />
             </Grid>
             <Grid item xs={12}>
                 <BlockEditor />
