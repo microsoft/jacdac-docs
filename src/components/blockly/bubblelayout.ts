22eeae38b6d8813c492e6a17ccdec01bd03443c2
pelikhan <jhalleux@microsoft.com>
2021-06-11 08:44:21 -0700

    start on layout

--- src/components/blockly/bubblelayout.ts
+++ src/components/blockly/bubblelayout.ts
@@ -1,17 +1,25 @@
-
 import { BlockSvg, WorkspaceSvg, Rect, Block } from "blockly"
-import {toMap } from "../../../jacdac-ts/src/jdom/utils"
+import { SMap, toMap } from "../../../jacdac-ts/src/jdom/utils"
 
 /**
  * Bubble layout adapted from Code Bubbles - https://web.eecs.utk.edu/~azh/cosc494spring2019/papers/Bragdon2010CHI_CodeBubbles.pdf
  */
-export function computeBubbleLayout(workspace: WorkspaceSvg, userMoved: BlockSvg) {
-    const topBlocks = workspace.getTopBlocks(false) as BlockSvg[];
+export function computeBubbleLayout(
+    workspace: WorkspaceSvg,
+    userMoved: BlockSvg
+) {
+    const topBlocks = workspace.getTopBlocks(false) as BlockSvg[]
 
     // precompute all bounding rectangle to avoid recomputations
-    const bubbles = toMap(topBlocks, b => b.id, b => b.getBoundingRectangle())
+    const bubbles = toMap(
+        topBlocks,
+        b => b.id,
+        b => b.getBoundingRectangle()
+    )
     // just moved set starts with user moved bubble
-    const justMoved: Set<string> = new Set([userMoved.id])
+    const justMoved = new Set([userMoved.id])
     // find unmarked bubbles that interset with userMoved
-    const unmarked = new Set<BlockSvg>(bubbles.filter(b)
+    const unmarked = new Set(
+        Object.keys(bubbles).filter(id => !justMoved.has(id))
+    )
 }
