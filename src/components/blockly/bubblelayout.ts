1f8cb4e99ad3c8d133e5169e4f011c4c48918c26
pelikhan <jhalleux@microsoft.com>
2021-06-11 08:53:55 -0700

    maps

--- src/components/blockly/bubblelayout.ts
+++ src/components/blockly/bubblelayout.ts
@@ -1,4 +1,4 @@
-import { BlockSvg, WorkspaceSvg, Rect, Block } from "blockly"
+import { BlockSvg, WorkspaceSvg, utils } from "blockly"
 import { SMap, toMap } from "../../../jacdac-ts/src/jdom/utils"
 
 /**
@@ -8,18 +8,30 @@ export function computeBubbleLayout(
     workspace: WorkspaceSvg,
     userMoved: BlockSvg
 ) {
+    const log = console.log
     const topBlocks = workspace.getTopBlocks(false) as BlockSvg[]
 
     // precompute all bounding rectangle to avoid recomputations
-    const bubbles = toMap(
+    const bubbles: SMap<utils.Rect> = toMap(
         topBlocks,
         b => b.id,
         b => b.getBoundingRectangle()
     )
     // just moved set starts with user moved bubble
-    const justMoved = new Set([userMoved.id])
+    const justMoved: string[] = [userMoved.id]
+
+    // determines if a bubble interset with the moved set
+    const intersectWithMoved = (id: string) => {
+        return justMoved.some(oid => bubbles[oid].intersects(bubbles[id]))
+    }
+
     // find unmarked bubbles that interset with userMoved
-    const unmarked = new Set(
-        Object.keys(bubbles).filter(id => !justMoved.has(id))
-    )
+    const unmarked: string[] = Object.keys(bubbles)
+        .filter(id => id !== userMoved.id)
+        .filter(id => intersectWithMoved(id))
+
+    if (!unmarked.length) {
+        log("bubbles: no initial overlap")
+        return []
+    }
 }
