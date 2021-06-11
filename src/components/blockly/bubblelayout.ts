232854c2aa1c442237c5e82a7b13912b8cf4f544
pelikhan <jhalleux@microsoft.com>
2021-06-11 09:37:00 -0700

    more progress

--- src/components/blockly/bubblelayout.ts
+++ src/components/blockly/bubblelayout.ts
@@ -20,15 +20,15 @@ export function computeBubbleLayout(
     // just moved set starts with user moved bubble
     const marked: string[] = [userMoved.id]
 
+    // determines if a bubble interset with the moved set
+    const intersectWithMarked = (id: string) => {
+        return marked.some(oid => bubbles[oid].intersects(bubbles[id]))
+    }
+
     // find unmarked bubbles that interset with userMoved
     const unmarked: string[] = Object.keys(bubbles)
         .filter(id => id !== userMoved.id)
-        .filter(id => intersectWithMoved(id))
-
-    // determines if a bubble interset with the moved set
-    const intersectWithMoved = (id: string) => {
-        return marked.some(oid => bubbles[oid].intersects(bubbles[id]))
-    }
+        .filter(id => intersectWithMarked(id))
 
     if (!unmarked.length) {
         log("bubbles: no initial overlap")
@@ -39,8 +39,28 @@ export function computeBubbleLayout(
 
     // generates 4 axis-aligned movements  (up, down, left, right) which move i the minimum distance such that i no longer intersects any marked bubbles.
     const next = unmarked.shift()
+    const nextr = bubbles[next]
+    const { left, right, top, bottom } = nextr
+
+    const mtop = marked.reduce(
+        (prev, m) => Math.max(prev, bubbles[m].top - top),
+        0
+    )
+    const mbottom = marked.reduce(
+        (prev, m) => Math.max(prev, bubbles[m].bottom - bottom),
+        0
+    )
+    const mleft = marked.reduce(
+        (prev, m) => Math.max(prev, bubbles[m].left - left),
+        0
+    )
+    const mright = marked.reduce(
+        (prev, m) => Math.max(prev, bubbles[m].right - right),
+        0
+    )
+    console.log("4axis", { nextr, mtop, mbottom, mleft, mright })
 
     // Movements that are in the opposite direction to that moved by
-    // any bubbles in J that intersect i are pruned (except on the initial call). The 2 movements with the largest Euclidian
-    // distances are kept, the rest are pruned.
+    // any bubbles in J that intersect i are pruned (except on the initial call). 
+    // The 2 movements with the largest Euclidian distances are kept, the rest are pruned.
 }
