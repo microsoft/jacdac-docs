becfdc9a67ec48cd87547952d7c128d534d0f439
pelikhan <jhalleux@microsoft.com>
2021-06-11 22:02:25 -0700

    pass through

--- src/components/blockly/dsl/chartdsl.ts
+++ src/components/blockly/dsl/chartdsl.ts
@@ -31,6 +31,7 @@ const chartDSL: BlockDomainSpecificLanguage = {
             colour,
             template: "meta",
             inputsInline: false,
+            transformData: b => b,
         },
     ],
 
