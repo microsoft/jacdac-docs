bda8a9570b234cdffd8a36f70ca9a88207af379a
pelikhan <jhalleux@microsoft.com>
2021-06-14 00:12:43 -0700

    error handling

--- src/components/blockly/dsl/workers/data.proxy.ts
+++ src/components/blockly/dsl/workers/data.proxy.ts
@@ -5,6 +5,8 @@ export default async function postTransformData(
     message: DataMessage
     // eslint-disable-next-line @typescript-eslint/ban-types
 ): Promise<object[]> {
+    // check for missing data
+    if (!message.data) return undefined
     const ws = workerProxy()
     message.jacdacdata = true
     const res = await ws.postMessage<DataMessage, DataMessage>(message)
