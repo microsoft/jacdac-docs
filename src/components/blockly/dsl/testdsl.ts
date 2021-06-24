4c4a47c25f026954cd52101c0ce2779b89372b17
pelikhan <jhalleux@microsoft.com>
2021-06-24 12:07:14 +0200

    more test blocks

--- src/components/blockly/dsl/testdsl.ts
+++ src/components/blockly/dsl/testdsl.ts
@@ -9,10 +9,14 @@ import {
     OptionsInputDefinition,
     TextInputDefinition,
     testColour,
+    ValueInputDefinition,
+    BOOLEAN_TYPE,
 } from "../toolbox"
 import BlockDomainSpecificLanguage from "./dsl"
 
 const TEST_BLOCK = "test_test"
+const TEST_ASK_BLOCK = "test_ask"
+const TEST_CHECK_BLOCK = "test_check"
 const colour = testColour
 
 const testDsl: BlockDomainSpecificLanguage = {
@@ -20,7 +24,7 @@ const testDsl: BlockDomainSpecificLanguage = {
     createBlocks: () => [
         {
             kind: "block",
-            type: "test_test",
+            type: TEST_BLOCK,
             message0: `test %1 for %2`,
             args0: [
                 <TextInputDefinition>{
@@ -45,9 +49,45 @@ const testDsl: BlockDomainSpecificLanguage = {
                 },
             ],
             colour,
-            inputsInline: false,
+            inputsInline: true,
             tooltip: `Defines a new test for a service`,
             helpUrl: "",
+            nextStatement: CODE_STATEMENT_TYPE,
+        },
+        {
+            kind: "block",
+            type: TEST_ASK_BLOCK,
+            message0: `ask %1`,
+            args0: [
+                <TextInputDefinition>{
+                    type: "field_input",
+                    name: "question",
+                    text: "a question",
+                },
+            ],
+            colour,
+            inputsInline: true,
+            tooltip: `Asks a question to the user.`,
+            helpUrl: "",
+            previousStatement: CODE_STATEMENT_TYPE,
+            nextStatement: CODE_STATEMENT_TYPE,
+        },
+        {
+            kind: "block",
+            type: TEST_CHECK_BLOCK,
+            message0: `check %1`,
+            args0: [
+                <ValueInputDefinition>{
+                    type: "input_value",
+                    name: "expression",
+                    check: BOOLEAN_TYPE,
+                },
+            ],
+            colour,
+            inputsInline: true,
+            tooltip: `Checks that a boolean condition is true.`,
+            helpUrl: "",
+            previousStatement: CODE_STATEMENT_TYPE,
             nextStatement: CODE_STATEMENT_TYPE,
         },
     ],
@@ -60,6 +100,14 @@ const testDsl: BlockDomainSpecificLanguage = {
                 {
                     kind: "block",
                     type: TEST_BLOCK,
+                },
+                {
+                    kind: "block",
+                    type: TEST_ASK_BLOCK,
+                },
+                {
+                    kind: "block",
+                    type: TEST_CHECK_BLOCK,
                 },
             ],
         },
