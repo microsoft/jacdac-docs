ff18d1cb56bf498934743f9bb88c4a38104f13ed
Tom Ball <you@example.com>
2021-06-28 08:23:15 -0700

    remove unneeded import

--- src/components/blockly/RoleChip.tsx
+++ src/components/blockly/RoleChip.tsx
@@ -4,7 +4,6 @@ import BlockContext from "./BlockContext"
 import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
 import DeviceAvatar from "../devices/DeviceAvatar"
 import {
-    serviceSpecificationFromClassIdentifier,
     serviceSpecificationFromName,
 } from "../../../jacdac-ts/src/jdom/spec"
 import { JDService } from "../../../jacdac-ts/src/jdom/service"
