789afcfba3ea9d2f254fcb8b667430239a4f4717
pelikhan <jhalleux@microsoft.com>
2021-06-30 01:01:39 -0700

    start-stop browser audio

--- src/pages/tools/accelerometer-theremin.tsx
+++ src/pages/tools/accelerometer-theremin.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useContext } from "react"
 import JacdacContext, { JacdacContextProps } from "../../jacdac/Context"
-import { createToneContext, ToneContext } from "../../components/hooks/toneContext"
 import { startServiceProviderFromServiceClass } from "../../../jacdac-ts/src/servers/servers"
 import {
     BuzzerCmd,
@@ -11,7 +10,6 @@ import {
 import { jdpack } from "../../../jacdac-ts/src/jdom/pack"
 import Packet from "../../../jacdac-ts/src/jdom/packet"
 import { throttle } from "../../../jacdac-ts/src/jdom/utils"
-import useServiceProviderFromServiceClass from "../../components/hooks/useServiceProviderFromServiceClass"
 import useServices from "../../components/hooks/useServices"
 import GridHeader from "../../components/ui/GridHeader"
 import { Button } from "gatsby-theme-material-ui"
@@ -25,9 +23,10 @@ import {
 import ConnectAlert from "../../components/alert/ConnectAlert"
 import DeviceCardHeader from "../../components/DeviceCardHeader"
 import { JDService } from "../../../jacdac-ts/src/jdom/service"
-import usePlayTone from "../../components/hooks/usePlayTone"
-import useServiceServer from "../../components/hooks/useServiceServer"
-import BuzzerServer from "../../../jacdac-ts/src/servers/buzzerserver"
+import BuzzerServer, {
+    BuzzerTone,
+} from "../../../jacdac-ts/src/servers/buzzerserver"
+import WebAudioContext from "../../components/ui/WebAudioContext"
 
 function tonePayload(frequency: number, ms: number, volume: number) {
     const period = Math.round(1000000 / frequency)
@@ -46,65 +45,65 @@ export default function AccelerometerTheremin() {
     const [accelService, setAccelService] = useState<JDService>()
     const [buzzerServer, setBuzzerServer] = useState<BuzzerServer>()
 
-    const { playTone, setVolume, onClickActivateAudioContext, activated } =
-        usePlayTone()
+    const { playTone, onClickActivateAudioContext, activated } =
+        useContext(WebAudioContext)
 
     // listen for playTone commands from the buzzer
     useEffect(
         () =>
-            buzzerServer?.subscribe<[number, number]>(
+            buzzerServer?.subscribe<BuzzerTone>(
                 BuzzerServer.PLAY_TONE,
-                args => {
-                    playTone?.(args[0], args[1])
-                }
+                ({ frequency, duration, volume }) =>
+                    playTone(frequency, duration, volume)
             ),
         [buzzerServer]
     )
+    // make sure to clean out buzzer server
+    useEffect(
+        () => () =>
+            buzzerServer?.device &&
+            bus.removeServiceProvider(buzzerServer.device),
+        [buzzerServer]
+    )
 
-    const handleSelectAccelerometerService = (accel) => () => {
+    const handleSelectAccelerometerService = accel => () => {
         accelService == accel ? setAccelService(null) : setAccelService(accel)
     }
 
-    const handleBrowserAudioEnable = () => () => { 
-        onClickActivateAudioContext();
-        const dev = startServiceProviderFromServiceClass(
-            bus,
-            SRV_BUZZER
-        )
-        const srv = dev
-            .services()
-            .find(s => s.serviceClass === SRV_BUZZER) as BuzzerServer
-
-        setBuzzerServer(srv);
+    const handleBrowserAudioEnable = () => {
+        onClickActivateAudioContext()
+        if (!buzzerServer) {
+            const dev = startServiceProviderFromServiceClass(bus, SRV_BUZZER)
+            const srv = dev
+                .services()
+                .find(s => s.serviceClass === SRV_BUZZER) as BuzzerServer
+            setBuzzerServer(srv)
+        } else {
+            setBuzzerServer(undefined)
+        }
     }
 
     // register for accelerometer data events
     useEffect(() => {
-        let unsubs = () => {}
-        if (accelService) 
-            unsubs = accelService.readingRegister.subscribe(
-                REPORT_UPDATE,
-                // don't trigger more than every 100ms
-                throttle(async () => {
-                    const [x] = accelService.readingRegister.unpackedValue
-                    await Promise.all(
-                        buzzers.map(async buzzer => {
-                            const pkt = Packet.from(
-                                BuzzerCmd.PlayTone,
-                                tonePayload(
-                                    1000 + (x * 1000),
-                                    TONE_DURATION,
-                                    1
-                                )
-                            )
-                            await buzzer.sendPacketAsync(pkt)
-                        })
-                    )
-                }, TONE_THROTTLE)
-            )
+        const unsubs = accelService?.readingRegister.subscribe(
+            REPORT_UPDATE,
+            // don't trigger more than every 100ms
+            throttle(async () => {
+                const [x] = accelService.readingRegister.unpackedValue
+                await Promise.all(
+                    buzzers.map(async buzzer => {
+                        const pkt = Packet.from(
+                            BuzzerCmd.PlayTone,
+                            tonePayload(1000 + x * 1000, TONE_DURATION, 1)
+                        )
+                        await buzzer.sendPacketAsync(pkt)
+                    })
+                )
+            }, TONE_THROTTLE)
+        )
 
         // cleanup callback
-        return () => unsubs()
+        return () => unsubs?.()
     }, [accelService, buzzers]) // re-register if accelerometers, buzzers change
 
     // TODO any specific rendering needed here?
@@ -112,12 +111,9 @@ export default function AccelerometerTheremin() {
         <Grid container spacing={2}>
             <Grid item xs={12}>
                 <GridHeader title="Audio controls" />
-                <Button
-                    variant={"outlined"}
-                    onClick={handleBrowserAudioEnable()}
-                >
-                    {activated 
-                        ? "Browser audio enabled"
+                <Button variant={"outlined"} onClick={handleBrowserAudioEnable}>
+                    {activated && buzzerServer
+                        ? "Stop browser audio"
                         : "Start browser audio"}
                 </Button>
             </Grid>
@@ -133,34 +129,48 @@ export default function AccelerometerTheremin() {
                 <>
                     <GridHeader title="Available accelerometers" />
                     {accelerometers.map(accelerometer => (
-                        <Grid item xs={12} sm={6} lg={4} xl={3} key={accelerometer.id}>
-                        <Card>
-                            <DeviceCardHeader
-                                device={accelerometer.device}
-                                showAvatar={true}
-                                showMedia={true}
-                            />
-                            <CardContent>
-                                <Typography variant="h5">
-                                    {(accelerometer === accelService
-                                        ? "Streaming from "
-                                        : "") + (accelerometer.device.physical ? "Physical " : "Virtual ") + `Accelerometer ${accelerometer.friendlyName}`}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button
-                                    variant={"outlined"}
-                                    onClick={handleSelectAccelerometerService(accelerometer)}
-                                >
-                                    {accelerometer === accelService
-                                        ? "Stop streaming"
-                                        : "Start streaming"}
-                                </Button>
-                            </CardActions>
-                        </Card>
+                        <Grid
+                            item
+                            xs={12}
+                            sm={6}
+                            lg={4}
+                            xl={3}
+                            key={accelerometer.id}
+                        >
+                            <Card>
+                                <DeviceCardHeader
+                                    device={accelerometer.device}
+                                    showAvatar={true}
+                                    showMedia={true}
+                                />
+                                <CardContent>
+                                    <Typography variant="h5">
+                                        {(accelerometer === accelService
+                                            ? "Streaming from "
+                                            : "") +
+                                            (accelerometer.device.physical
+                                                ? "Physical "
+                                                : "Virtual ") +
+                                            `Accelerometer ${accelerometer.friendlyName}`}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button
+                                        variant={"outlined"}
+                                        onClick={handleSelectAccelerometerService(
+                                            accelerometer
+                                        )}
+                                    >
+                                        {accelerometer === accelService
+                                            ? "Stop streaming"
+                                            : "Start streaming"}
+                                    </Button>
+                                </CardActions>
+                            </Card>
                         </Grid>
                     ))}
                 </>
-                )}
-        </Grid>)
+            )}
+        </Grid>
+    )
 }
