24b770263ff6e7208a5d3db0d4aca972fbd8aab5
pelikhan <jhalleux@microsoft.com>
2021-07-01 12:24:51 +0200

    adding hooks to handle directories

---
+++ src/components/hooks/useDirectoryHandle.ts
@@ -0,0 +1,60 @@
+import { useContext, useState } from "react"
+import useChange from "../../jacdac/useChange"
+import DbContext from "../DbContext"
+
+async function verifyPermission(fileHandle: FileSystemHandle) {
+    if (!fileHandle) return false
+
+    const options: FileSystemHandlePermissionDescriptor = {
+        mode: "readwrite",
+    }
+    // Check if permission was already granted. If so, return true.
+    if ((await fileHandle.queryPermission(options)) === "granted") {
+        return true
+    }
+    // Request permission. If the user grants permission, return true.
+    if ((await fileHandle.requestPermission(options)) === "granted") {
+        return true
+    }
+    // The user didn't grant permission, so return false.
+    return false
+}
+
+export default function useDirectoryHandle(storageKey: string) {
+    const { db } = useContext(DbContext)
+    const directories = useChange(db, _ => _?.directories)
+    const [directory, setDirectory] = useState<FileSystemDirectoryHandle>()
+
+    const supported =
+        typeof window !== "undefined" && !!window.showDirectoryPicker
+    const showDirectoryPicker = supported
+        ? async (options?: DirectoryPickerOptions) => {
+              const dir = await window.showDirectoryPicker(options)
+              directories.set(storageKey, dir)
+          }
+        : undefined
+    useChange(
+        directories,
+        async () => {
+            let dir = await directories?.get(storageKey)
+            console.log(`load directory ${storageKey}`, { dir })
+            if (dir) {
+                // check permissions
+                const perm = await verifyPermission(dir)
+                if (!perm) {
+                    console.log(`permission verification failed`)
+                    // clear from db
+                    directories.set(storageKey, undefined)
+                    dir = undefined
+                }
+            }
+            setDirectory(dir)
+        },
+        [storageKey]
+    )
+    return {
+        supported,
+        showDirectoryPicker,
+        directory,
+    }
+}
