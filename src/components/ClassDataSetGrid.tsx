64228d9cad068d1652fd1387f280e84acaecbaf4
rwilliams-msr <randi@DESKTOP-2THDOED.localdomain>
2021-07-21 21:00:17 -0400

    Updating file format

--- src/components/ClassDataSetGrid.tsx
+++ src/components/ClassDataSetGrid.tsx
@@ -1,49 +1,50 @@
-import { Card, CardActions, CardContent, CardHeader, Grid } from "@material-ui/core";
-import { Button, IconButton } from "gatsby-theme-material-ui";
-import React, { useContext } from "react";
-import { prettyDuration } from "../../jacdac-ts/src/jdom/pretty";
-import ServiceManagerContext from "./ServiceManagerContext";
-import Trend from "./Trend";
-import useGridBreakpoints from './useGridBreakpoints';
-// tslint:disable-next-line: no-submodule-imports match-default-export-name
-import SaveAltIcon from '@material-ui/icons/SaveAlt';
-// tslint:disable-next-line: no-submodule-imports match-default-export-name
-import DeleteIcon from '@material-ui/icons/Delete';
-import FieldDataSet from "./FieldDataSet";
+import React, { useContext } from "react"
 
+import { Card, CardContent, CardHeader, Grid } from "@material-ui/core"
+import { IconButton } from "gatsby-theme-material-ui"
+import DeleteIcon from "@material-ui/icons/Delete"
+// tslint:disable-next-line: no-submodule-imports match-default-export-name
+
+import Trend from "./Trend"
+import FieldDataSet from "./FieldDataSet"
+import ServiceManagerContext from "./ServiceManagerContext"
 
 export default function ClassDataSetGrid(props: {
-    label: string,
-    tables: FieldDataSet[],
+    label: string
+    tables: FieldDataSet[]
     handleDeleteTable?: (table: FieldDataSet) => void
 }) {
-    const { label, tables, handleDeleteTable } = props;
+    const { label, tables, handleDeleteTable } = props
     const { fileStorage } = useContext(ServiceManagerContext)
-    const gridBreakpoints = useGridBreakpoints(tables?.length)
-    
+
     const handleDownload = (table: FieldDataSet) => () => {
-        const sep = ','
+        const sep = ","
         const csv = table.toCSV(sep)
         fileStorage.saveText(`${table.name}.csv`, csv)
     }
     const handleDelete = (table: FieldDataSet) => () => handleDeleteTable(table)
 
-    return <Grid container spacing={2}>
-        <Card>
-            <CardHeader
-                subheader={label + `, ${tables.length} sample(s)`} />
-            <CardContent>
-            <Grid container spacing={1}>
-            {tables.map((table) =>
-               <Grid item key={`result` + table.id}>
-                            <Trend dataSet={table} height={8} mini={true} />
-                            {handleDeleteTable &&
+    return (
+        <Grid container spacing={2}>
+            <Card>
+                <CardHeader
+                    subheader={label + `, ${tables.length} sample(s)`}
+                />
+                <CardContent>
+                    <Grid container spacing={1}>
+                        {tables.map(table => (
+                            <Grid item key={`result` + table.id}>
+                                <Trend dataSet={table} height={8} mini={true} />
+                                {handleDeleteTable && (
                                     <IconButton onClick={handleDelete(table)}>
                                         <DeleteIcon />
-                                    </IconButton>}
-                    </Grid>)}
-                </Grid>
-            </CardContent>
-        </Card>
-    </Grid>
+                                    </IconButton>
+                                )}
+                            </Grid>
+                        ))}
+                    </Grid>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
 }
