db573dc85fd565e525e2575d15cd4161d59acbcb
Jennifer Mankoff <jmankoff@cs.washington.edu>
2021-07-27 16:26:00 -0700

    Fixed useEffect and setup call to new modular object

--- src/pages/tools/audio-controls.tsx
+++ src/pages/tools/audio-controls.tsx
@@ -44,36 +44,35 @@ type AudioControlProps = {
     sensorSelection: (a: JDService) => null;
 };
 
-// register for light sensor data events
-// JM: just tried to move this over from lightsensor-modular
-// JM: but I'm not sure how to parameterize it with audioControlProps... thus the errors
-// JM: stopped here
-useEffect(() => {
-        // ?. checks that the callee is defined
-        const unsubs = sensor?.readingRegister.subscribe(
-            REPORT_UPDATE,
-            // don't trigger more than every 100ms
-            throttle(async () => {
-                // get amount of light
-                //console.log(lightService.readingRegister.unpackedValue)
-                const [sensorValue] = sensorService.readingRegister.unpackedValue
-                if(sonificationProperty == 'frequency')
-                {
-                    setToneFrequencyOfset(sensorValue)
-                } else{
-                    setVolume(sensorValue%0.99)  
-                }
-                
-                await playTone(1000 + toneFrequencyOffset * 1000, TONE_DURATION, volume)
-            }, TONE_THROTTLE)
-        )
-
-        // cleanup callback
-    return () => unsubs?.()
-}, [sensorService, playTone]) // re-register if light sensor changes
-
 const AudioControls =  ({sensorTitle, sensor, sensorClass, sensorService, sensorSelection}:AudioControlProps) => {
-  return (<>
+    // register for light sensor data events
+    // JM: just tried to move this over from lightsensor-modular
+    // JM: but I'm not sure how to parameterize it with audioControlProps... thus the errors
+    // JM: stopped here
+    useEffect(() => {
+    // ?. checks that the callee is defined
+    const unsubs = sensor?.readingRegister.subscribe(
+        REPORT_UPDATE,
+        // don't trigger more than every 100ms
+        throttle(async () => {
+            // get amount of light
+            //console.log(lightService.readingRegister.unpackedValue)
+            const [sensorValue] = sensorService.readingRegister.unpackedValue
+            if(sonificationProperty == 'frequency')
+            {
+                setToneFrequencyOfset(sensorValue)
+            } else{
+                setVolume(sensorValue%0.99)  
+            }
+            
+            await playTone(1000 + toneFrequencyOffset * 1000, TONE_DURATION, volume)
+        }, TONE_THROTTLE)
+    )
+
+    // cleanup callback
+    return () => unsubs?.()
+    }, [sensorService, playTone]) // re-register if light sensor changes
+    return (<>
       <section id={sectionId}>
       <Grid container spacing={2}>
             <GridHeader title="Audio controls"/>
