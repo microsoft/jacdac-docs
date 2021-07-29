7b606bcf9bc0a41d23f8b2293ea46f593bf2eb0f
pelikhan <jhalleux@microsoft.com>
2021-07-29 13:13:33 -0700

    added blocks to do encoding

--- src/components/blockly/dsl/vegadsl.ts
+++ src/components/blockly/dsl/vegadsl.ts
@@ -1,14 +1,21 @@
+import DataColumnChooserField from "../fields/DataColumnChooserField"
 import {
     BlockReference,
     CategoryDefinition,
+    DataColumnInputDefinition,
     DATA_SCIENCE_STATEMENT_TYPE,
+    DummyInputDefinition,
     identityTransformData,
+    OptionsInputDefinition,
+    StatementInputDefinition,
 } from "../toolbox"
 import BlockDomainSpecificLanguage from "./dsl"
-import DataTableField from "../fields/DataTableField"
 import { paletteColorByIndex } from "./palette"
 
-const VEGA_CHART_BLOCK = "vega_chart"
+const VEGA_LAYER_BLOCK = "vega_layer"
+const VEGA_ENCODING_BLOCK = "vega_encoding"
+const VEGA_STATEMENT_TYPE = "vegaStatementType"
+const VEGA_ENCODING_OPTIONS_TYPE = "vegaEncodingOptionStatementType"
 
 const colour = paletteColorByIndex(5)
 const vegaDsl: BlockDomainSpecificLanguage = {
@@ -16,16 +23,78 @@ const vegaDsl: BlockDomainSpecificLanguage = {
     createBlocks: () => [
         {
             kind: "block",
-            type: VEGA_CHART_BLOCK,
-            message0: "vega chart %1",
+            type: VEGA_LAYER_BLOCK,
+            message0: "chart %1 %2 %3",
             args0: [
-                {
-                    type: DataTableField.KEY,
-                    name: "table",
+                <OptionsInputDefinition>{
+                    type: "field_dropdown",
+                    options: [
+                        "area",
+                        "bar",
+                        "circle",
+                        "line",
+                        "point",
+                        "rect",
+                        "rule",
+                        "square",
+                        "text",
+                        "tick",
+                    ].map(s => [s, s]),
+                    name: "mark",
+                },
+                <DummyInputDefinition>{
+                    type: "input_dummy",
+                },
+                <StatementInputDefinition>{
+                    type: "input_statement",
+                    name: "fields",
+                    check: VEGA_STATEMENT_TYPE,
                 },
             ],
             previousStatement: DATA_SCIENCE_STATEMENT_TYPE,
             nextStatement: DATA_SCIENCE_STATEMENT_TYPE,
+            colour,
+            template: "meta",
+            inputsInline: false,
+            transformData: identityTransformData,
+        },
+        {
+            kind: "block",
+            type: VEGA_ENCODING_BLOCK,
+            message0: "encoding %1 with field %2 %3 %4",
+            args0: [
+                <OptionsInputDefinition>{
+                    type: "field_dropdown",
+                    options: [
+                        "x",
+                        "y",
+                        "x2",
+                        "y2",
+                        "xError",
+                        "yError",
+                        "xError2",
+                        "yError2",
+                        "theta",
+                        "theta2",
+                        "radius",
+                        "radius2",
+                    ].map(s => [s, s]),
+                },
+                <DataColumnInputDefinition>{
+                    type: DataColumnChooserField.KEY,
+                    name: "field",
+                },
+                <DummyInputDefinition>{
+                    type: "input_dummy",
+                },
+                <StatementInputDefinition>{
+                    type: "input_statement",
+                    name: "fields",
+                    check: VEGA_ENCODING_OPTIONS_TYPE,
+                },                
+            ],
+            previousStatement: VEGA_STATEMENT_TYPE,
+            nextStatement: VEGA_STATEMENT_TYPE,
             colour,
             template: "meta",
             inputsInline: false,
@@ -37,7 +106,8 @@ const vegaDsl: BlockDomainSpecificLanguage = {
             kind: "category",
             name: "Vega",
             contents: [
-                <BlockReference>{ kind: "block", type: VEGA_CHART_BLOCK },
+                <BlockReference>{ kind: "block", type: VEGA_LAYER_BLOCK },
+                <BlockReference>{ kind: "block", type: VEGA_ENCODING_BLOCK },
             ],
             colour,
         },
