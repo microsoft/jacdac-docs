ef692955a3aa50fd8aacb9b0c735b60fee464f50
pelikhan <jhalleux@microsoft.com>
2021-08-02 13:38:34 +0200

    towards auto-ui

--- src/components/ui/JSONSchemaForm.tsx
+++ src/components/ui/JSONSchemaForm.tsx
@@ -1,24 +1,103 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import React from "react"
-import MaterialJsonSchemaForm from "react-jsonschema-form-material-ui"
 import type { JSONSchema4 } from "json-schema"
+import { Grid, TextField } from "@material-ui/core"
+import SwitchWithLabel from "./SwitchWithLabel"
 
-export default function JSONSchemaForm(props: {
+function TypeForm(props: {
     schema: JSONSchema4
-    // eslint-disable-next-line @typescript-eslint/ban-types
     value: object
-    // eslint-disable-next-line @typescript-eslint/ban-types
     setValue: (newValue: object) => void
 }) {
     const { schema, value, setValue } = props
+    const { type, title, description } = schema
+
+    switch (type) {
+        case "number":
+        case "integer":
+            return (
+                <Grid item xs={12}>
+                    <TextField
+                        title={title}
+                        helperText={description}
+                        type="number"
+                        value={value}
+                        onChange={setValue}
+                    />
+                </Grid>
+            )
+        case "string":
+            return (
+                <Grid item xs={12}>
+                    <TextField
+                        title={title}
+                        helperText={description}
+                        type="text"
+                        value={value}
+                        onChange={setValue}
+                    />
+                </Grid>
+            )
+        case "boolean":
+            return (
+                <Grid item xs={12}>
+                    <SwitchWithLabel
+                        title={title}
+                        label={description}
+                        checked={!!value}
+                        onChange={setValue}
+                    />
+                </Grid>
+            )
+    }
+
+    return null
+}
+
+function PropertiesForm(props: {
+    properties: { [index: string]: JSONSchema4 }
+    value: object
+    setValue: (newValue: object) => void
+}) {
+    const { properties, value, setValue } = props
+    const handleSetValue =
+        (key: string, value: object) => (newValue: object) => {
+            setValue({ ...value, key: newValue })
+        }
 
     return (
-        <MaterialJsonSchemaForm
-            schema={schema}
-            formData={value}
-            onChange={setValue}
-            onSubmit={submittedData =>
-                console.log("form submitted", submittedData)
-            }
-        />
+        <>
+            {Object.entries(properties).map(([key, schema]) => (
+                <Grid item key={key}>
+                    <TypeForm
+                        schema={schema}
+                        value={value[key]}
+                        setValue={handleSetValue(key, value)}
+                    />
+                </Grid>
+            ))}
+        </>
+    )
+}
+
+export default function JSONSchemaForm(props: {
+    schema: JSONSchema4
+    value: object
+    setValue: (newValue: object) => void
+}) {
+    const { schema, value, setValue } = props
+    const { properties } = schema
+
+    return (
+        <Grid container spacing={1}>
+            <TypeForm schema={schema} value={value} setValue={setValue} />
+            {properties && (
+                <PropertiesForm
+                    properties={properties}
+                    value={value}
+                    setValue={setValue}
+                />
+            )}
+        </Grid>
     )
 }
