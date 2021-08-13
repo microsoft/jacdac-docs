a315509ed6ce320fd16380c51a88a5f909907bcf
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-08-13 17:19:28 +0000

    added ui

--- src/components/ui/useUnitConverter.ts
+++ src/components/ui/useUnitConverter.ts
@@ -2,6 +2,29 @@
 /// <reference path="../../../jacdac-ts/jacdac-spec/spectool/jdspec.d.ts" />
 import useLocalStorage from "../hooks/useLocalStorage"
 
+const adapters: Record<string, Record<string, (v: number) => number>> = {
+    "°C": {
+        "°C": v => v,
+        "°F": v => (5 / 9) * v + 32,
+    },
+}
+
+export function unitConverters() {
+    const [settings, setSettings] = useLocalStorage("jacdac:unitconverters", {})
+    return {
+        converters: Object.keys(adapters).map(unit => ({
+            unit,
+            name: settings[unit] || unit,
+            names: Object.keys(adapters[unit]),
+        })),
+        setConverter: (unit: string, name: string) => {
+            const newSettings = { ...settings }
+            newSettings[unit] = name
+            setSettings(`new using ${name}`)
+        },
+    }
+}
+
 export default function useUnitConverter(unit: jdspec.Unit) {
     if (!unit)
         return {
@@ -9,12 +32,6 @@ export default function useUnitConverter(unit: jdspec.Unit) {
         }
 
     const [settings, setSettings] = useLocalStorage("jacdac:unitconverters", {})
-    const adapters: Record<string, Record<string, (v: number) => number>> = {
-        "°C": {
-            "°C": v => v,
-            "°F": v => (5 / 9) * v + 32,
-        },
-    }
 
     const adapter = adapters[unit]
     if (!adapter)
