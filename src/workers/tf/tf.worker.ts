986dff0b861bba380c0fbd3e929fe40b9759ca26
pelikhan <jhalleux@microsoft.com>
2021-08-31 12:47:05 -0700

    import type only for LayerStats

--- src/workers/tf/tf.worker.ts
+++ src/workers/tf/tf.worker.ts
@@ -12,7 +12,8 @@ import {
 
 import type { Tensor, Sequential } from "@tensorflow/tfjs"
 
-import { LayerStats, compileAndTest, compileModel } from "../../../ml4f"
+import { compileAndTest, compileModel } from "../../../ml4f"
+import type { LayerStats } from "../../../ml4f"
 export type { LayerStats } from "../../../ml4f"
 
 export interface TFModelObj {
