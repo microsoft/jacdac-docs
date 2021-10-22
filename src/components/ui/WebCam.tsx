6f6f362b9e95b9db840081d8ce5fca920c08f59d
pelikhan <jhalleux@microsoft.com>
2021-10-22 14:38:49 -0700

    somewhat working

--- src/components/ui/WebCam.tsx
+++ src/components/ui/WebCam.tsx
@@ -3,15 +3,29 @@ import {
     Card,
     CardContent,
     CardHeader,
+    createStyles,
+    FormControl,
+    makeStyles,
     MenuItem,
     Select,
 } from "@material-ui/core"
-import React, { ChangeEvent, useEffect, useRef, useState } from "react"
+import React, {
+    ChangeEvent,
+    lazy,
+    useContext,
+    useEffect,
+    useRef,
+    useState,
+} from "react"
 import useLocalStorage from "../hooks/useLocalStorage"
 import useEffectAsync from "../useEffectAsync"
 import SettingsIcon from "@material-ui/icons/Settings"
 import IconButtonWithTooltip from "./IconButtonWithTooltip"
 import useMounted from "../hooks/useMounted"
+import Suspense from "./Suspense"
+import CloseIcon from "@material-ui/icons/Close"
+import AppContext from "../AppContext"
+const Draggable = lazy(() => import("react-draggable"))
 
 export function isMediaDevicesSupported(): boolean {
     return (
@@ -22,14 +36,41 @@ export function isMediaDevicesSupported(): boolean {
     )
 }
 
+const useStyles = makeStyles(() =>
+    createStyles({
+        cardContainer: {
+            zIndex: 1100,
+            position: "absolute",
+            left: "5rem",
+            top: "5rem",
+        },
+        card: {
+            "& .hostedcontainer": {
+                position: "relative",
+                width: "20rem",
+            },
+            "& iframe": {
+                border: "none",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+            },
+        },
+    })
+)
+
 export default function WebCam() {
-    const [devices, setDevices] = useState<MediaDeviceInfo[]>("")
-    const [deviceId, setDeviceId] = useLocalStorage("webcam_deviceid")
+    const { setShowWebCam } = useContext(AppContext)
+    const [devices, setDevices] = useState<MediaDeviceInfo[]>()
+    const [deviceId, setDeviceId] = useLocalStorage("webcam_deviceid", "")
+    const nodeRef = useRef<HTMLSpanElement>()
     const streamRef = useRef<MediaStream>()
     const videoRef = useRef<HTMLVideoElement>()
-    const [settingsOpen, setSettingsOpen] = useState(false)
+    const [settingsOpen, setSettingsOpen] = useState(!deviceId)
     const mounted = useMounted()
+    const classes = useStyles()
 
+    const handleClose = () => setShowWebCam(false)
     const handleSettings = () => setSettingsOpen(newValue => !newValue)
     const handleDeviceChange = (
         ev: ChangeEvent<{ name?: string; value: unknown }>
@@ -99,38 +140,71 @@ export default function WebCam() {
 
     // cleanup
     useEffect(() => stop, [deviceId])
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const draggableProps: any = {
+        nodeRef,
+    }
 
     return (
-        <>
-            <Card>
-                <CardHeader
-                    action={
-                        <IconButtonWithTooltip
-                            onClick={handleSettings}
-                            title="Settings"
-                        >
-                            <SettingsIcon />
-                        </IconButtonWithTooltip>
-                    }
-                />
-                <CardContent>
-                    {settingsOpen && (
-                        <Select
-                            title="select a webcam"
-                            open={settingsOpen}
-                            onChange={handleDeviceChange}
-                            value={deviceId}
-                        >
-                            {devices?.map(({ deviceId, label }) => (
-                                <MenuItem key={deviceId} value={deviceId}>
-                                    {label}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    )}
-                    <video autoPlay playsInline ref={videoRef} title="webcam" />
-                </CardContent>
-            </Card>
-        </>
+        <Suspense>
+            <Draggable {...draggableProps}>
+                <span ref={nodeRef} className={classes.cardContainer}>
+                    <Card className={classes.card}>
+                        <CardHeader
+                            title={
+                                settingsOpen && (
+                                    <FormControl
+                                        variant="outlined"
+                                        size="small"
+                                    >
+                                        <Select
+                                            title="select a webcam"
+                                            open={settingsOpen}
+                                            onChange={handleDeviceChange}
+                                            value={deviceId}
+                                        >
+                                            {devices?.map(
+                                                ({ deviceId, label }) => (
+                                                    <MenuItem
+                                                        key={deviceId}
+                                                        value={deviceId}
+                                                    >
+                                                        {label}
+                                                    </MenuItem>
+                                                )
+                                            )}
+                                        </Select>
+                                    </FormControl>
+                                )
+                            }
+                            action={
+                                <>
+                                    <IconButtonWithTooltip
+                                        onClick={handleSettings}
+                                        title="Settings"
+                                    >
+                                        <SettingsIcon />
+                                    </IconButtonWithTooltip>
+                                    <IconButtonWithTooltip
+                                        onClick={handleClose}
+                                        title="Close"
+                                    >
+                                        <CloseIcon />
+                                    </IconButtonWithTooltip>
+                                </>
+                            }
+                        />
+                        <CardContent>
+                            <video
+                                autoPlay
+                                playsInline
+                                ref={videoRef}
+                                title="webcam"
+                            />
+                        </CardContent>
+                    </Card>
+                </span>
+            </Draggable>
+        </Suspense>
     )
 }
