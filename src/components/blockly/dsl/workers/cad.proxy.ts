61cb400e836f5f8afaff29db8a5f38af799c0458
pelikhan <jhalleux@microsoft.com>
2021-10-30 09:41:38 -0700

    fixing types

--- src/components/blockly/dsl/workers/cad.proxy.ts
+++ src/components/blockly/dsl/workers/cad.proxy.ts
@@ -1,19 +1,26 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import workerProxy from "./proxy"
 import type {
+    CadConvertRequest,
+    CadConvertResponse,
+} from "../../../../workers/cad/dist/node_modules/cad.worker"
+import type {
     EnclosureModel,
     EnclosureOptions,
-    CadConvertRequest,
-    CadConvertResponse
-} from "../../../../workers/cad/dist/node_modules/cad.worker"
+} from "../../../../workers/cad/dist/node_modules/enclosurecad"
 
-export async function convertToSTL(model: EnclosureModel, options?: EnclosureOptions): Promise<Blob> {
+export async function convertToSTL(
+    model: EnclosureModel,
+    options?: EnclosureOptions
+): Promise<Blob> {
     const worker = workerProxy("csv")
-    const res = await worker.postMessage<CadConvertRequest, CadConvertResponse>({
-        worker: "cad",
-        type: "convert",
-        model,
-        options,
-    })
+    const res = await worker.postMessage<CadConvertRequest, CadConvertResponse>(
+        {
+            worker: "cad",
+            type: "convert",
+            model,
+            options,
+        }
+    )
     return res?.stl
 }
