27a4bd154788a55bb8d4a414d03ce1ee2fc2adb8
pelikhan <jhalleux@microsoft.com>
2021-10-30 01:09:12 -0700

    stl rendering

--- src/components/home/models/STLModel.tsx
+++ src/components/home/models/STLModel.tsx
@@ -2,14 +2,14 @@ import React, { Suspense } from "react"
 import { useLoader } from "@react-three/fiber"
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader"
 
-export default function STLModel(props: { url: string }) {
-    const { url } = props
+export default function STLModel(props: { url: string; color?: string }) {
+    const { url, color } = props
     const obj = useLoader(STLLoader, url)
     return (
         <Suspense fallback={null}>
             <mesh>
                 <primitive object={obj} attach="geometry" />
-                <meshStandardMaterial color={"green"} />
+                <meshStandardMaterial color={color || "green"} />
             </mesh>
         </Suspense>
     )
--- src/pages/tools/enclosure.tsx
+++ src/pages/tools/enclosure.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import EnclosureGenerator from "../../components/tools/EnclosureGenerator"
+import EnclosureEditor from "../../components/tools/EnclosureEditor"
 
 export default function Page() {
-    return <EnclosureGenerator />
+    return <EnclosureEditor />
 }
