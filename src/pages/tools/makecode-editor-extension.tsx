b79457cc8f968a4c2bf8064170da9a21d0915f30
pelikhan <jhalleux@microsoft.com>
2021-11-02 21:26:07 -0700

    clean ou

--- src/pages/tools/makecode-editor-extension.tsx
+++ src/pages/tools/makecode-editor-extension.tsx
@@ -1,28 +1,31 @@
 import React, { useContext, useEffect } from "react"
+import { styled } from "@mui/material/styles"
 import { createTheme, responsiveFontSizes } from "@mui/material"
-import createStyles from "@mui/styles/createStyles"
 import ThemedLayout from "../../components/ui/ThemedLayout"
 import MakeCodeEditorExtension from "../../components/makecode/MakeCodeEditorExtension"
-import makeStyles from "@mui/styles/makeStyles"
 import PaperBox from "../../components/ui/PaperBox"
 import DarkModeContext from "../../components/ui/DarkModeContext"
 
-const useStyles = makeStyles(theme =>
-    createStyles({
-        content: {
-            display: "flex",
-            minHeight: "100vh",
-            minWidth: "10rem",
-            flexDirection: "column",
-            padding: theme.spacing(3),
-            transition: theme.transitions.create("margin", {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            flexGrow: 1,
-        },
-    })
-)
+const PREFIX = "makecode-editor-extension"
+
+const classes = {
+    content: `${PREFIX}-content`,
+}
+
+const Root = styled("div")(({ theme }) => ({
+    [`& .${classes.content}`]: {
+        display: "flex",
+        minHeight: "100vh",
+        minWidth: "10rem",
+        flexDirection: "column",
+        padding: theme.spacing(3),
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        flexGrow: 1,
+    },
+}))
 
 export default function MakeCodeEditorExtensionPage() {
     const { toggleDarkMode, darkModeMounted } = useContext(DarkModeContext)
@@ -41,18 +44,20 @@ export default function MakeCodeEditorExtensionPage() {
             mode: "light",
         },
     })
-    const classes = useStyles()
+
     const theme = responsiveFontSizes(rawTheme)
     useEffect(() => {
         if (darkModeMounted) toggleDarkMode("light")
     }, [darkModeMounted])
     return (
-        <ThemedLayout theme={theme}>
-            <div className={classes.content}>
-                <PaperBox>
-                    <MakeCodeEditorExtension />
-                </PaperBox>
-            </div>
-        </ThemedLayout>
+        <Root>
+            <ThemedLayout theme={theme}>
+                <div className={classes.content}>
+                    <PaperBox>
+                        <MakeCodeEditorExtension />
+                    </PaperBox>
+                </div>
+            </ThemedLayout>
+        </Root>
     )
 }
