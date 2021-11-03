6c5010e18a1f7e1093ae3e53fa99c69093458007
pelikhan <jhalleux@microsoft.com>
2021-11-02 21:49:11 -0700

    more theming

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -1,13 +1,12 @@
-import { Theme, Tooltip as MaterialTooltip } from "@mui/material"
-
-import withStyles from "@mui/styles/withStyles"
+import { Tooltip as MaterialTooltip } from "@mui/material"
+import { styled } from "@mui/material/styles"
 
 // fix for contrast issue
-const Tooltip = withStyles((theme: Theme) => ({
-    tooltip: {
+const Tooltip = styled(MaterialTooltip)(({ theme }) => ({
+    [`& .tooltip`]: {
         backgroundColor: theme.palette.background.default,
         color: theme.palette.text.primary,
     },
-}))(MaterialTooltip)
+}))
 
 export default Tooltip
