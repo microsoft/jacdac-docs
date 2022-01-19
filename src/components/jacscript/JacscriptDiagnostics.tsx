c737b8f3065e3769ef6178b9dc4096e9b9e220fc
pelikhan <jhalleux@microsoft.com>
2022-01-19 14:46:31 -0800

    show diagnostics

--- src/components/jacscript/JacscriptDiagnostics.tsx
+++ src/components/jacscript/JacscriptDiagnostics.tsx
@@ -1,20 +1,19 @@
-import { Grid, Typography } from "@mui/material"
+import { Grid } from "@mui/material"
 import React from "react"
 import CodeBlock from "../CodeBlock"
 import {JacScriptProgram} from "../../../jacdac-ts/src/vm/ir2jacscript"
 
 export default function JacscriptDiagnostics(props: { program: JacScriptProgram }) {
     const { program } = props
-    if (!program) return null    
     return (
         <Grid item xs={12}>
-            <Typography variant="subtitle1">Jacscript</Typography>
+            <h3>Jacscript</h3>
             <CodeBlock
                 className="javascript"
                 downloadName={"test.jcs.json"}
-                downloadText={JSON.stringify(program, null, 2)}
+                downloadText={program && JSON.stringify(program, null, 2)}
             >
-                {program.program.join("\n")}
+                {program?.program.join("\n") || "--"}
             </CodeBlock>
         </Grid>
     )
