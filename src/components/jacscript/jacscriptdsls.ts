3ea942a8a13c1966436e61973d56ea20c89c7209
pelikhan <jhalleux@microsoft.com>
2022-01-19 14:55:55 -0800

    remove blocks

--- src/components/jacscript/jacscriptdsls.ts
+++ src/components/jacscript/jacscriptdsls.ts
@@ -1,5 +1,4 @@
 import servicesDSL from "../blockly/dsl/servicesdsl"
-import loopsDsl from "../blockly/dsl/loopsdsl"
 import logicDsl from "../blockly/dsl/logicdsl"
 import mathDsl from "../blockly/dsl/mathdsl"
 import shadowDsl from "../blockly/dsl/shadowdsl"
@@ -7,7 +6,6 @@ import fieldsDsl from "../blockly/dsl/fieldsdsl"
 
 const jacscriptDsls = [
     servicesDSL,
-    loopsDsl,
     logicDsl,
     mathDsl,
     shadowDsl,
