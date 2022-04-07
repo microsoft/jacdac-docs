a5955ad472d222d226f88de0174e5ea62a93db13
pelikhan <jhalleux@microsoft.com>
2022-04-07 12:55:14 -0700

    unlist setting service

--- src/components/shell/ToolsDrawer.tsx
+++ src/components/shell/ToolsDrawer.tsx
@@ -25,8 +25,6 @@ import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt"
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
 import WifiIcon from "@mui/icons-material/Wifi"
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
-import SettingsIcon from "@mui/icons-material/Settings"
-// tslint:disable-next-line: no-submodule-imports match-default-export-name
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord"
 import BugReportIcon from "@mui/icons-material/BugReport"
 import VideoCallIcon from "@mui/icons-material/VideoCall"
@@ -172,11 +170,6 @@ export default function ToolsDrawer() {
             icon: <WifiIcon />,
         },
         {},
-        {
-            text: "Device Settings",
-            to: "/tools/settings/",
-            icon: <SettingsIcon />,
-        },
         {
             text: "Firmware Update",
             to: "/tools/updater/",
