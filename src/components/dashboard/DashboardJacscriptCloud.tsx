510be8b0bdc6db180816db51a524432b5e247163
pelikhan <jhalleux@microsoft.com>
2022-05-11 17:14:50 -0700

    jscript cloud

---
+++ src/components/dashboard/DashboardJacscriptCloud.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react"
+import { Button, Grid, Typography } from "@mui/material"
+// tslint:disable-next-line: no-submodule-imports match-default-export-name
+// tslint:disable-next-line: no-submodule-imports match-default-export-name
+import { DashboardServiceProps } from "./DashboardServiceWidget"
+import useServiceServer from "../hooks/useServiceServer"
+import {
+    JacscriptCloudServer,
+    JacscriptCloudUploadRequest,
+    UPLOAD,
+} from "../../../jacdac-ts/src/servers/jacscriptcloudserver"
+import {
+    JacscriptCloudCmd,
+    JacscriptCloudReg,
+} from "../../../jacdac-ts/src/jdom/constants"
+import useRegister from "../hooks/useRegister"
+import { useRegisterBoolValue } from "../../jacdac/useRegisterValue"
+import SwitchWithLabel from "../ui/SwitchWithLabel"
+import DashboardRegisterValueFallback from "./DashboardRegisterValueFallback"
+import CmdButton from "../CmdButton"
+
+export default function DashboardJacscriptManager(
+    props: DashboardServiceProps
+) {
+    const { service, expanded } = props
+
+    const connectedRegister = useRegister(service, JacscriptCloudReg.Connected)
+    const connected = useRegisterBoolValue(connectedRegister, props)
+    const [uploads, setUploads] = useState<JacscriptCloudUploadRequest[]>([])
+    const server = useServiceServer(service, () => new JacscriptCloudServer())
+    const color = server ? "secondary" : "primary"
+
+    useEffect(() => setUploads([]), [connectedRegister])
+    useEffect(
+        () =>
+            server?.subscribe(UPLOAD, (req: JacscriptCloudUploadRequest) => {
+                setUploads(prev => [...prev, req].slice(-10))
+            }),
+        [server]
+    )
+
+    const handleConnected = server
+        ? () => {
+              server.connected = !server.connected
+              connectedRegister.scheduleRefresh()
+          }
+        : undefined
+
+    const handleUpload = async () => {
+        const label = "test"
+        const args = [1, 2, 3]
+        await service.sendCmdPackedAsync(JacscriptCloudCmd.Upload, [
+            label,
+            args.map(n => [n]),
+        ])
+    }
+
+    if (connected === undefined)
+        return <DashboardRegisterValueFallback register={connectedRegister} />
+
+    return (
+        <Grid container spacing={1}>
+            <Grid item xs={12}>
+                <Typography variant="caption">Jacscript Cloud</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <SwitchWithLabel
+                    checked={connected}
+                    label="connected"
+                    readOnly={!server}
+                    color={color}
+                    onChange={handleConnected}
+                />
+            </Grid>
+            {!!uploads?.length && (
+                <Grid item xs={12}>
+                    <pre>
+                        {uploads.map(
+                            ({ label, args }) =>
+                                `${label}: ${args.join(", ")}\n`
+                        )}
+                    </pre>
+                </Grid>
+            )}
+            {expanded && (
+                <Grid item xs={12}>
+                    <CmdButton
+                        variant="outlined"
+                        onClick={handleUpload}
+                        disabled={!connected}
+                    >
+                        upload
+                    </CmdButton>
+                </Grid>
+            )}
+        </Grid>
+    )
+}
