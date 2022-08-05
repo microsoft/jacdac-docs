ecdd01b70769216475d4ee495ba4673951d0d103
pelikhan <jhalleux@microsoft.com>
2022-08-05 10:15:32 -0700

    adding gps

---
+++ src/components/dashboard/DashboardSatNav.tsx
@@ -0,0 +1,7 @@
+import { DashboardServiceProps } from "./DashboardServiceWidget"
+
+export default function DashboardSatNav(props: DashboardServiceProps) {
+    const { service } = props
+
+    return null
+}
