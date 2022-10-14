4715be514e0abdd80e0ee915483f7463b659ded0
pelikhan <jhalleux@microsoft.com>
2022-10-14 14:34:10 -0700

    more migration to brains

--- src/components/jacscript/JacscriptEditorToolbar.tsx
+++ src/components/jacscript/JacscriptEditorToolbar.tsx
@@ -1,31 +1,17 @@
 import { Grid } from "@mui/material"
 import React from "react"
 import BlockRolesToolbar from "../blockly/BlockRolesToolbar"
-import { WORKSPACE_FILENAME } from "../blockly/toolbox"
-import FileTabs from "../fs/FileTabs"
-import useFileSystem from "../FileSystemContext"
-import { JACSCRIPT_NEW_FILE_CONTENT } from "./JacscriptEditor"
 import JacscriptManagerChipItems from "./JacscriptManagerChipItems"
+import useBrainManagerState from "../brains/useBrainManagerState"
 
 export default function JacscriptEditorToolbar() {
-    const { fileSystem } = useFileSystem()
+    const { state } = useBrainManagerState()
 
     return (
-        <>
-            {!!fileSystem && (
-                <Grid item xs={12}>
-                    <FileTabs
-                        newFileName={WORKSPACE_FILENAME}
-                        newFileContent={JACSCRIPT_NEW_FILE_CONTENT}
-                        hideFiles={true}
-                    />
-                </Grid>
-            )}
-            <Grid item xs={12}>
-                <BlockRolesToolbar>
-                    <JacscriptManagerChipItems />
-                </BlockRolesToolbar>
-            </Grid>
-        </>
+        <Grid item xs={12}>
+            <BlockRolesToolbar>
+                <JacscriptManagerChipItems />
+            </BlockRolesToolbar>
+        </Grid>
     )
 }
