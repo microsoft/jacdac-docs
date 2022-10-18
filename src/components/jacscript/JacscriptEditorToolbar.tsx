43d59e3b2b2ca1d42b5a94e3b3749576f89a676d
pelikhan <jhalleux@microsoft.com>
2022-10-18 10:56:06 -0700

    adding toolbar

--- src/components/jacscript/JacscriptEditorToolbar.tsx
+++ src/components/jacscript/JacscriptEditorToolbar.tsx
@@ -1,16 +1,71 @@
-import { Grid } from "@mui/material"
+import { Grid, MenuItem, SelectChangeEvent } from "@mui/material"
 import React from "react"
 import BlockRolesToolbar from "../blockly/BlockRolesToolbar"
 import JacscriptManagerChipItems from "./JacscriptManagerChipItems"
 import { useBrainManager } from "../brains/BrainManagerContext"
+import { Button } from "gatsby-theme-material-ui"
+import CloudUploadIcon from "@mui/icons-material/CloudUpload"
+import SelectWithLabel from "../ui/SelectWithLabel"
+
+function SelectProgram() {
+    const { state, programId, setProgramId } = useBrainManager()
+    const { programs } = state || {}
+    const handleChange = (ev: SelectChangeEvent<string>) => {
+        const id = ev.target.value
+        setProgramId(id)
+    }
+    return (
+        <SelectWithLabel
+            label="Program"
+            placeholder="Select a program"
+            value={programId}
+            fullWidth
+            size="small"
+            onChange={handleChange}
+        >
+            {programs?.map(program => (
+                <MenuItem key={program.id} value={program.id}>
+                    {program.name}
+                </MenuItem>
+            ))}
+        </SelectWithLabel>
+    )
+}
+
+function UploadProgramButton() {
+    const handleUpload = () => {}
+    return (
+        <Button
+            startIcon={<CloudUploadIcon />}
+            onClick={handleUpload}
+            variant="outlined"
+        >
+            Upload
+        </Button>
+    )
+}
+
+function BrainManagerToolbar() {
+    return (
+        <Grid sx={{ mt: 0.5, mb: 0.5 }} container direction="row" spacing={1}>
+            <Grid item xs>
+                <SelectProgram />
+            </Grid>
+            <Grid item>
+                <UploadProgramButton />
+            </Grid>
+        </Grid>
+    )
+}
 
 export default function JacscriptEditorToolbar() {
     const { token } = useBrainManager()
+
     return (
         <>
             {token && (
                 <Grid item xs={12}>
-                    Jacscript manager
+                    <BrainManagerToolbar />
                 </Grid>
             )}
             <Grid item xs={12}>
