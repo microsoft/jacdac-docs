70de665703df20b37bef2d2d28cd8d1620ec42f8
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-04-06 03:42:40 +0000

    removing blockly

--- src/components/blockly/dsl/fieldsdsl.ts
+++
@@ -1,8 +0,0 @@
-import { fieldShadows } from "../fields/fields"
-import BlockDomainSpecificLanguage from "./dsl"
-
-const fieldsDsl: BlockDomainSpecificLanguage = {
-    id: "fields",
-    createBlocks: () => fieldShadows(),
-}
-export default fieldsDsl
