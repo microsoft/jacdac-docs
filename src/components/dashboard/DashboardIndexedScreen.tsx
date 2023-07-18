edafaaf7704accb07e50bc9faa9aa6e4e1f84807
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-07-18 07:08:45 +0000

    more support for sims

--- src/components/dashboard/DashboardIndexedScreen.tsx
+++ src/components/dashboard/DashboardIndexedScreen.tsx
@@ -2,16 +2,16 @@ import React, { CSSProperties, useEffect, useId, useRef } from "react"
 import { DashboardServiceProps } from "./DashboardServiceWidget"
 import useServiceServer from "../hooks/useServiceServer"
 import { IndexedScreenServer } from "../../../jacdac-ts/src/servers/indexedscreenserver"
-import { IndexedScreenReg, RENDER } from "../../../jacdac-ts/src/jdom/constants"
+import { CHANGE, IndexedScreenReg } from "../../../jacdac-ts/src/jdom/constants"
 import useRegister from "../hooks/useRegister"
 import { useRegisterUnpackedValue } from "../../jacdac/useRegisterValue"
 import DashboardRegisterValueFallback from "./DashboardRegisterValueFallback"
-import useBus from "../../jacdac/useBus"
+
+const MIN_OPACITY = 0.4
 
 export default function DashboardLEDStrip(props: DashboardServiceProps) {
     const { service, visible } = props
     const id = useId()
-    const bus = useBus()
     const server = useServiceServer<IndexedScreenServer>(
         service,
         () => new IndexedScreenServer()
@@ -19,13 +19,36 @@ export default function DashboardLEDStrip(props: DashboardServiceProps) {
 
     const widthRegister = useRegister(service, IndexedScreenReg.Width)
     const heightRegister = useRegister(service, IndexedScreenReg.Height)
+    const brightnessRegister = useRegister(service, IndexedScreenReg.Brightness)
+    const rotationRegister = useRegister(service, IndexedScreenReg.Rotation)
+    const paletteRegister = useRegister(service, IndexedScreenReg.Palette)
+    const bitsPerPixelRegister = useRegister(
+        service,
+        IndexedScreenReg.BitsPerPixel
+    )
 
     const [width] = useRegisterUnpackedValue<[number]>(widthRegister, props)
     const [height] = useRegisterUnpackedValue<[number]>(heightRegister, props)
+    const [brightness = 1] = useRegisterUnpackedValue<[number]>(
+        brightnessRegister,
+        props
+    )
+    const [rotation = 0] = useRegisterUnpackedValue<[number]>(
+        rotationRegister,
+        props
+    )
+    const [palette] = useRegisterUnpackedValue<
+        [[number, number, number, number][]]
+    >(paletteRegister, props)
+    const [bitsPerPixel] = useRegisterUnpackedValue<[number]>(
+        bitsPerPixelRegister,
+        props
+    )
 
     const canvasRef = useRef<HTMLCanvasElement>()
     const contextRef = useRef<CanvasRenderingContext2D>()
 
+    // update canvas size
     useEffect(() => {
         contextRef.current = undefined
         const canvas = canvasRef.current
@@ -35,29 +58,26 @@ export default function DashboardLEDStrip(props: DashboardServiceProps) {
         }
     }, [width, height])
 
-    const paint = () => {
-        const canvas = canvasRef.current
-        if (!canvas) return
-
-        const context =
-            contextRef.current || (contextRef.current = canvas.getContext("2d"))
-        if (!context) return
-
-        console.log("RENDER")
-        // paint image
-        context.fillStyle = "#ccc"
-        context.fillRect(0, 0, canvas.width, canvas.height)
-    }
-
+    // repaint
     useEffect(
-        () => visible && bus?.subscribe(RENDER, paint),
-        [bus, visible, service, server]
-    )
+        () =>
+            visible &&
+            server?.subscribe(CHANGE, () => {
+                const canvas = canvasRef.current
+                if (!canvas) return
 
-    const style: CSSProperties = {
-        minWidth: "10rem",
-        width: "100%"
-    }
+                const context =
+                    contextRef.current ||
+                    (contextRef.current = canvas.getContext("2d"))
+                if (!context) return
+
+                console.log("RENDER")
+                // paint image
+                context.fillStyle = "#333"
+                context.fillRect(0, 0, canvas.width, canvas.height)
+            }),
+        [server, visible, width, height, bitsPerPixel, palette]
+    )
 
     if (width === undefined || height === undefined)
         return (
@@ -66,5 +86,11 @@ export default function DashboardLEDStrip(props: DashboardServiceProps) {
             />
         )
 
+    const style: CSSProperties = {
+        minWidth: "10rem",
+        width: "100%",
+        opacity: MIN_OPACITY + brightness * (1 - MIN_OPACITY),
+    }
+    if (rotation) style.transform = `rotate(${rotation}deg)`
     return <canvas id={id} ref={canvasRef} style={style} />
 }
