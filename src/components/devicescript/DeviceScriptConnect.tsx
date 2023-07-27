98132f3ad140ee3e3cf8c49c511c60230a4c4541
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-07-27 12:40:28 +0000

    added title to connect page

--- src/components/devicescript/DeviceScriptConnect.tsx
+++ src/components/devicescript/DeviceScriptConnect.tsx
@@ -1,16 +1,22 @@
 import React from "react"
 import Console from "../console/Console"
+import { Typography } from "@mui/material"
 
 export default function DeviceScriptConnect() {
     return (
-        <Console
-            hook={true}
-            showConnect={true}
-            showToolbar={true}
-            showFiles={false}
-            showLevel={true}
-            showPopout={false}
-            showSerial={false}
-        />
+        <>
+            <Typography component="h1" variant="h6">
+                DeviceScript Connection Helper
+            </Typography>
+            <Console
+                hook={true}
+                showConnect={true}
+                showToolbar={true}
+                showFiles={false}
+                showLevel={false}
+                showPopout={false}
+                showSerial={false}
+            />
+        </>
     )
 }
