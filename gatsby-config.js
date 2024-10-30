25c2673eeb83d94564d21ed2782ea09a2287154c
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-10-30 21:58:26 +0000

    add HSTS headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ const SITE_TITLE = `Jacdac - Connect and code electronics. Instantly.`
 const SITE_DESCRIPTION = `Jacdac is a plug-and-play hardware and software stack for microcontrollers and their peripherals such as sensors and actuators. Jacdac is primarily designed for “modular electronics” scenarios that support rapid prototyping, creative exploration, making and learning through physical computing. Jacdac is designed to be cheap, flexible and extensible.`
 
 module.exports = {
+    headers: [{
+        source: `/*`,
+        headers: [{
+            key: `Strict-Transport-Security`, value: `max-age=360000`
+        }]
+    }],
     trailingSlash: "always",
     siteMetadata: {
         title: SITE_TITLE,
